d47e26b0928fc4fb3c473ac4e1feb21847fd9ecf
zamu134 <117544767+zamu134@users.noreply.github.com>
2023-03-18 10:10:04 -0600

    Add files via upload

---
+++ Base-de_datos-saul-main/Base-de_datos-Almacen-main/Asincrona_s8_Almacen-main/Asincrona_s8_Almacen/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asincrona_s8_Almacen.Models;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public decimal? Precio { get; set; }
+
+    public int? Stock { get; set; }
+}
---
+++ Base-de_datos-saul-main/Base-de_datos-Almacen-main/Asincrona_s8_Almacen-main/Asincrona_s8_Almacen/Program.cs
@@ -0,0 +1,40 @@
+﻿using Asincrona_s8_Almacen.Models;
+
+Console.WriteLine("          ---------------------------------------------------------------------       ");
+Console.WriteLine("             Bienvenido a almacenes roock          ");
+Console.WriteLine("          ---------------------------------------------------------------------       ");
+
+AlmacenContext db = new AlmacenContext();
+Producto producto = new Producto();
+
+Console.WriteLine();
+
+Console.Write("Ingrese el nombre de el producto ");
+producto.Nombre = Console.ReadLine();
+Console.Write("Describa el producto ");
+producto.Descripcion = Console.ReadLine();
+Console.Write("Ingrese el precio que tendra el producto ");
+producto.Precio = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Ingrese la cantidad de productos de stock ");
+producto.Stock = Convert.ToInt32(Console.ReadLine());
+
+db.Producto.Add(producto);
+db.SaveChanges();
+
+Console.WriteLine("   el producto ha sido registrado con exito   ");
+Console.WriteLine(" ...........................................");
+Console.WriteLine(" Id Cantidad. Productos Descripcion Precio ");
+Console.WriteLine(" ...........................................");
+
+Console.WriteLine();
+
+var ListaProductos = db.Producto.ToList();
+foreach (var product in ListaProductos){
+
+    Console.WriteLine($"  {product.Id}  {product.Stock}    {product.Nombre} / {product.Descripcion} / ${product.Precio}");
+}
+
+
+Console.WriteLine("-------------------------------------------------------------------------");
+Console.WriteLine("         Gracias por preferirnos como su tienda online          ");
+Console.WriteLine("-------------------------------------------------------------------------");
